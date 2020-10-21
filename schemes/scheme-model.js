f867799f0e9e979b873893f184a80bfa02f8a326
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-20 23:23:39 -0400

    completed scheme-model

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -1,13 +1,25 @@
-const db = require("../data/config")
+const db = require("../data/config");
 
-function find () {
-    return db("schemes")
-};
-
-function findById (id) {
-   return db("schemes").where({ id }).first()
-     
-};
-module.exports = {
-    find, findById,
+function find() {
+  return db("schemes");
 }
+function findById(id) {
+  return db("schemes").where({ id }).first();
+}
+function findSteps(id) {
+  return db("steps").where("steps.scheme_id", id).orderBy("steps.step_number");
+}
+function update(changes, id) {
+  return db("schemes").where({ id }).update(changes);
+}
+function remove(id) {
+  return db("schemes").where({ id }).del();
+}
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  update,
+  remove,
+};
