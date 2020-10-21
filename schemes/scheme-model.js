1ce8c7427e9dca3beeef03a01119c1ee970e4313
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-10-20 21:29:52 -0400

    created config file and added find and findById to model

---
+++ schemes/scheme-model.js
@@ -0,0 +1,13 @@
+const db = require("../data/config")
+
+function find () {
+    return db("schemes")
+};
+
+function findById (id) {
+   return db("schemes").where({ id }).first()
+     
+};
+module.exports = {
+    find, findById,
+}
